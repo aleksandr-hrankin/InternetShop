b2b6c61f86930589c4f19c0826b1d69f5062f2ad
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-09-07 16:17:58 +0300

    change completeOrder method in OrderServiceImpl class

--- src/main/java/com/internet/shop/service/impl/OrderServiceImpl.java
+++ src/main/java/com/internet/shop/service/impl/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import com.internet.shop.lib.Service;
 import com.internet.shop.model.Order;
 import com.internet.shop.model.ShoppingCart;
 import com.internet.shop.service.OrderService;
+import com.internet.shop.service.ShoppingCartService;
 import java.util.List;
 
 @Service
@@ -13,11 +14,14 @@ public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
 
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
         Order order = new Order(shoppingCart.getUserId());
         order.setProducts(List.copyOf(shoppingCart.getProducts()));
-        shoppingCart.getProducts().clear();
+        shoppingCartService.clear(shoppingCart);
         return orderDao.create(order);
     }
 
