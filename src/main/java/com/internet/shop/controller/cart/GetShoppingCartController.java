fe1f1199df4511e63bdbaf3cbcb99e7be0ea7e5a
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-09-09 22:45:30 +0300

    Added controllers

---
+++ src/main/java/com/internet/shop/controller/cart/GetShoppingCartController.java
@@ -0,0 +1,26 @@
+package com.internet.shop.controller.cart;
+
+import com.internet.shop.lib.Injector;
+import com.internet.shop.model.ShoppingCart;
+import com.internet.shop.service.interfaces.ShoppingCartService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetShoppingCartController extends HttpServlet {
+    private static final Long USER_ID = 1L;
+    private static final Injector injector = Injector.getInstance("com.internet.shop");
+    private ShoppingCartService shoppingCartService =
+            (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ShoppingCart shoppingCart = shoppingCartService.getByUserId(USER_ID);
+        req.setAttribute("shoppingCart", shoppingCart);
+        req.getRequestDispatcher("/WEB-INF/view/shopping-cart/productsInCart.jsp")
+                .forward(req, resp);
+    }
+}
