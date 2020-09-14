c68e310708ab1df6eab672389a9766cea0de96f1
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-09-14 11:57:08 +0300

    Added const in LoginController class

--- src/main/java/com/internet/shop/controller/LoginController.java
+++ src/main/java/com/internet/shop/controller/LoginController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class LoginController extends HttpServlet {
+    private static final String USER_ID = "user_id";
     private static final Injector inject = Injector.getInstance("com.internet.shop");
     private AuthenticationService authenticationService
             = (AuthenticationService) inject.getInstance(AuthenticationService.class);
@@ -30,7 +31,7 @@ public class LoginController extends HttpServlet {
         try {
             User user = authenticationService.login(login, password);
             HttpSession session = req.getSession();
-            session.setAttribute("user_id", user.getId());
+            session.setAttribute(USER_ID, user.getId());
         } catch (AuthenticationException e) {
             req.setAttribute("errorMessage", e.getMessage());
             req.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(req, resp);
